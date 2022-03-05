4e9879278cda938a757f3ecf616f4a3ac3b11294
bkcheung <b.cheung@hotmail.com>
2022-03-05 00:54:01 -0800

    Add page initialization js

---
+++ index.main.js
@@ -0,0 +1,49 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module './initializePage.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n// function pageLoadPrint(){\n//     const content = document.getElementById('content');\n//     const title = document.createElement('h1');\n//     const header = document.createElement('h3');\n//     const desc = document.createElement('p');\n\n//     title.innerHTML = \"Kevin's Pastas\";\n//     header.innerHTML = \"There's nothing quite like a hot bowl of Kevin's famous pastas.\";\n//     desc.innerHTML = \"Ever since he was a young wee lad, Kevin had a dream. A dream to make delicious pasta accessible to all. With the opening of Kevin's Pastas, in Haye's Valley, he brings his life-long dream to fruition\";\n\n//     content.appendChild(title);\n//     content.appendChild(header);\n//     content.appendChild(desc);\n// }\n\n\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module './initializePage.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())();\n\n//# sourceURL=webpack://restaurant/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
