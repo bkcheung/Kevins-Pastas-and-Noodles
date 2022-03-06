74e6884daa125af244e00a3d064654a0f4051ffb
bkcheung <b.cheung@hotmail.com>
2022-03-06 15:53:33 -0800

    Add tab switching event listener

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import loadMenu from './menu.js';
 import loadContact from './contact.js';
 import './style.css';
 
-// loadHome();
-// loadMenu();
-loadContact();
+function clearContent(){
+    document.getElementById('content').innerHTML = '';
+}
+
+loadHome();
+
+document.getElementById('home').addEventListener("click", () => {
+    clearContent();
+    loadHome();
+});
+document.getElementById('menu').addEventListener("click", () => {
+    clearContent();
+    loadMenu();
+});
+document.getElementById('contact').addEventListener("click", () => {
+    clearContent();
+    loadContact();
+});
