baeaf5438834ed86d56bb403050442d5c242897e
bkcheung <b.cheung@hotmail.com>
2022-03-06 16:37:12 -0800

    Update styling

--- src/home.js
+++ src/home.js
@@ -11,7 +11,7 @@ export default function loadHome(){
 
     title.innerHTML = "Kevin's Pastas and Noodles";
     header.innerHTML = "There's nothing quite like a hot bowl of Kevin's famous pastas and noodles.";
-    desc.innerHTML = "Ever since he was a young wee lad, Kevin had a dream. A dream to make delicious food accessible to all. With the opening of Kevin's Pastas and Noodles in Hayes Valley, he brings his life-long dream to fruition.";
+    desc.innerHTML = "Ever since he was a young wee lad, Kevin had a dream. A dream to make delicious food accessible to all. <br/><br/> With the opening of Kevin's Pastas and Noodles in Hayes Valley, he brings his life-long dream to fruition.";
 
     content.append(title, header, desc, pastaImage);
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import loadMenu from './menu.js';
 import loadContact from './contact.js';
 import './style.css';
 
+const homeButton = document.createElement('button');
+const menuButton = document.createElement('button');
+const contactButton = document.createElement('button');
+
 function loadHeader(){
     const header = document.getElementById('header')
-    const homeButton = document.createElement('button');
-    const menuButton = document.createElement('button');
-    const contactButton = document.createElement('button');
-
+    
     homeButton.setAttribute('id', 'home');
     menuButton.setAttribute('id', 'menu');
     contactButton.setAttribute('id', 'contact');
@@ -16,6 +17,8 @@ function loadHeader(){
     homeButton.textContent = 'Home';
     menuButton.textContent = 'Menu';
     contactButton.textContent = 'Contact';
+    homeButton.classList.add('selected');
+
 
     header.append(homeButton, menuButton, contactButton);
 }
@@ -32,19 +35,25 @@ function initPage(){
 
 function clearContent(){
     document.getElementById('content').innerHTML = '';
+    homeButton.classList.remove('selected');
+    menuButton.classList.remove('selected');
+    contactButton.classList.remove('selected');
 }
 
 initPage();
 
-document.getElementById('home').addEventListener("click", () => {
+homeButton.addEventListener("click", () => {
     clearContent();
     loadHome();
+    homeButton.classList.add('selected');
 });
-document.getElementById('menu').addEventListener("click", () => {
+menuButton.addEventListener("click", () => {
     clearContent();
     loadMenu();
+    menuButton.classList.add('selected');
 });
-document.getElementById('contact').addEventListener("click", () => {
+contactButton.addEventListener("click", () => {
     clearContent();
     loadContact();
+    contactButton.classList.add('selected');
 });
